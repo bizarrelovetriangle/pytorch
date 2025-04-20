6ebe1ba9f84975acfb657c4f8557df145e0dcf42
Расул Расулов <rasul.rasulov@glidewell.com>
2025-04-20 14:12:10 +0300

    something

--- GAN.py
+++ GAN.py
@@ -25,20 +25,57 @@ batch_size = 128
 path = "C:\\Users\\PC\\Downloads\\celeba"
 netPath = pathlib.Path(__file__).parent.resolve()
 
+class SkipConnection(torch.nn.Module):
+    def __init__(self, features):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.BatchNorm2d(features),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(features, features, 5, stride = 1, padding=(2, 2), output_padding=(0, 0)),
+            nn.BatchNorm2d(features),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(features, features, 5, stride = 1, padding=(2, 2), output_padding=(0, 0)),
+        )
+    
+    def forward(self, input):
+        return self.model(input) + input
+
+class DiscriminatorNet:
+    def __init__(self):
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 24, 5, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(24, 24, 5, stride=2),
+            nn.ReLU(),
+            SkipConnection(24),
+            nn.ReLU(),
+            nn.Conv2d(24, 36, 5, stride=2),
+            SkipConnection(36),
+            nn.ReLU(),
+            nn.Conv2d(36, 68, 5, stride=3),
+            SkipConnection(68),
+            nn.Conv2d(68, 1, kernel_size=(7, 5)),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, input):
+        return self.model(input)
+
 class EncoderNet:
     def __init__(self):
         self.model = nn.Sequential(
             nn.Conv2d(3, 24, 5, stride=2),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
+            nn.Conv2d(24, 24, 5, stride=2),
+            nn.ReLU(),
+            SkipConnection(24),
+            nn.ReLU(),
             nn.Conv2d(24, 36, 5, stride=2),
+            SkipConnection(36),
             nn.ReLU(),
             nn.Conv2d(36, 68, 5, stride=3),
-            nn.MaxPool2d(2, 2),
-            nn.Flatten(1),
-            nn.Linear(612, 200),
-            nn.ReLU(),
-            nn.Linear(200, latentSize),
+            SkipConnection(68),
+            nn.Conv2d(68, latentSize, kernel_size=(7, 5)),
             nn.Sigmoid()
         )
     
@@ -48,17 +85,19 @@ class EncoderNet:
 class DecoderNet:
     def __init__(self):
         self.model = nn.Sequential(
-            nn.Unflatten(-1, (latentSize, 1, 1)),
             nn.ConvTranspose2d(latentSize, 72, 5, stride = 1, padding=0, output_padding=0),
             nn.BatchNorm2d(72),
             nn.LeakyReLU(),
             nn.ConvTranspose2d(72, 36, 5, stride = 3, padding=0, output_padding=0),
+            SkipConnection(36),
             nn.BatchNorm2d(36),
             nn.LeakyReLU(),
             nn.ConvTranspose2d(36, 24, 5, stride = 3, padding=(0, 5), output_padding=(0, 0)),
+            SkipConnection(24),
             nn.BatchNorm2d(24),
             nn.LeakyReLU(),
             nn.ConvTranspose2d(24, 24, 5, stride = 2, padding=1, output_padding=0),
+            SkipConnection(24),
             nn.BatchNorm2d(24),
             nn.LeakyReLU(),
             nn.ConvTranspose2d(24, 3, 5, stride = 2, padding=0, output_padding=1),
@@ -71,6 +110,10 @@ class DecoderNet:
 if __name__ == '__main__':
     device = torch.device("cuda:0")
 
+    discriminator = DiscriminatorNet()
+    discriminator.model.to(device)
+    discriminator.model = nn.DataParallel(discriminator.model, list(range(1)))
+
     encoder = EncoderNet()
     encoder.model.to(device)
     encoder.model = nn.DataParallel(encoder.model, list(range(1)))
@@ -79,25 +122,26 @@ if __name__ == '__main__':
     decoder.model.to(device)
     decoder.model = nn.DataParallel(decoder.model, list(range(1)))
 
-    encoder.model.load_state_dict(torch.load(netPath / 'encoder.pth', weights_only=True))
-    decoder.model.load_state_dict(torch.load(netPath / 'decoder.pth', weights_only=True))
-    print(f'loaded from {netPath}')
+    #summary(discriminator.model, input_size=(batch_size, 3, imageHeight, imageWidth))
+    #summary(encoder.model, input_size=(batch_size, 3, imageHeight, imageWidth))
+    #summary(decoder.model, input_size=(batch_size, latentSize, 1, 1))
+    #exit()
+
+    #discriminator.model.load_state_dict(torch.load(netPath / 'discriminator.pth', weights_only=True))
+    #encoder.model.load_state_dict(torch.load(netPath / 'encoder.pth', weights_only=True))
+    #decoder.model.load_state_dict(torch.load(netPath / 'decoder.pth', weights_only=True))
+    #print(f'loaded from {netPath}')
     
-    image = torchvision.io.read_image("C:\\Users\\PC\\Desktop\\Test.png")
-    image = ((image[0:3, :, :].float() / 256) - 0.5) * 2
 
-    images = image[None, :, :, :]
-    encoded = encoder.model(images)
-    print(encoded.shape)
-    output = decoder.model(encoded).detach().cpu()
-    print(output.shape)
-    cat = torch.cat((images[:1], output[:1]), 0)
-    imshow(torchvision.utils.make_grid(cat))
-
-    exit()
-
-    #summary(encoder.model, input_size=(batch_size, 3, imageWidth, imageHeight))
-    #summary(decoder.model, input_size=(batch_size, latentSize))
+    #image = torchvision.io.read_image("C:\\Users\\PC\\Desktop\\Test.png")
+    #image = ((image[0:3, :, :].float() / 256) - 0.5) * 2
+    #images = image[None, :, :, :]
+    #encoded = encoder.model(images)
+    #print(encoded.shape)
+    #output = decoder.model(encoded).detach().cpu()
+    #print(output.shape)
+    #cat = torch.cat((images[:1], output[:1]), 0)
+    #imshow(torchvision.utils.make_grid(cat))
     #exit()
 
     transform = transforms.Compose([
@@ -108,50 +152,69 @@ if __name__ == '__main__':
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                              shuffle=True, num_workers=2)
 
-
-    #dataIter = next(iter(dataloader))
-    #batch = dataIter[0]
-    #image = batch[0]
-    #output = encoder.model(batch)
-    #output2 = decoder.model(output)
-    #print(image.shape)
-    #print(output.shape)
-    #print(output2.shape)
-    #imshow(torchvision.utils.make_grid(batch, 5))
-    #imshow(torchvision.utils.make_grid(output2, 5))
-    #exit()
-
-    train = False
+    train = True
 
     if train == True:
+        discriminatorCriterion = nn.BCELoss()
+        discriminatorOpt = torch.optim.Adam(discriminator.model.parameters(), betas=(0.5, 0.999))
         encoderOpt = torch.optim.Adam(encoder.model.parameters(), betas=(0.5, 0.999))
         decoderOpt = torch.optim.Adam(decoder.model.parameters(), betas=(0.5, 0.999))
 
-        runningLoss = 0.0
+        cycleConsistentLoss = 0.0
+        discriminatorLoss = 0.0
         
-        for epoch in range(500):
+        for epoch in range(1):
             for i, batch in enumerate(dataloader):
+                discriminatorOpt.zero_grad()
                 encoderOpt.zero_grad()
                 decoderOpt.zero_grad()
 
                 inputs = batch[0].to(device)
+
                 encoded = encoder.model(inputs)
                 output = decoder.model(encoded)
-                loss = torch.abs(inputs - output).sum()
 
-                loss.backward()
+                # labels
+                trueLabels = torch.full((inputs.size(0), 1, 1, 1), 1, dtype=torch.float32, device=device)
+                fakeLabels = torch.full((inputs.size(0), 1, 1, 1), 0, dtype=torch.float32, device=device)
+
+                # calculating losses for the discriminator
+                discriminatorReals = discriminator.model(inputs)
+                discriminatorRealError = discriminatorCriterion(discriminatorReals, trueLabels)
+                discriminatorRealError.backward()
+                
+                discriminatorFakeOutputs = discriminator.model(output.detach())
+                discriminatorFakeError = discriminatorCriterion(discriminatorFakeOutputs, fakeLabels)
+                discriminatorFakeError.backward()
+
+                #discriminatorOpt.step()
+
+                # calculating losses for the generator
+                #cycleConsistentError = torch.abs(inputs - output).sum()
+                #cycleConsistentError.backward(retain_graph=True)
+
+                discriminatorFakeOutputs2 = discriminator.model(output)
+                discriminatorFakeError2 = discriminatorCriterion(discriminatorFakeOutputs2, trueLabels)
+                discriminatorFakeError2.backward()
+
                 encoderOpt.step()
                 decoderOpt.step()
 
-                runningLoss += loss.item()
-                if i % 100 == 99:
-                    print(f'[epoch - {epoch}, {i}] - running loss: {runningLoss / 1000:.3f}')
-                    runningLoss = 0.0
+                discriminatorLoss += discriminatorRealError.item() + discriminatorFakeError.item()
+                #cycleConsistentLoss += cycleConsistentError.item()
+                #if i % 100 == 99:
+                print(f'[epoch - {epoch}, {i}/{len(dataloader)}] - cycleConsistentLoss: {cycleConsistentLoss / 100:>10.3f}, discriminatorLoss: {discriminatorLoss / 100:<10.3f}')
+                print(f'Discriminator real mean: {discriminatorReals.view(-1).mean():.6f}, Discriminator fake mean: {discriminatorFakeOutputs.view(-1).mean():.6f}')
+                print(f'discriminatorRealError: {discriminatorRealError:.6f}, discriminatorFakeError: {discriminatorFakeError:.6f}')
+                discriminatorLoss = 0.0
+                cycleConsistentLoss = 0.0
             
-            if epoch % 10 == 9:
-                torch.save(encoder.model.state_dict(), netPath / 'encoder.pth')
-                torch.save(decoder.model.state_dict(), netPath / 'decoder.pth')
-                print(f'safed to {netPath}')
+    #if epoch % 10 == 9:
+        torch.save(discriminator.model.state_dict(), netPath / 'discriminator.pth')
+        torch.save(encoder.model.state_dict(), netPath / 'encoder.pth')
+        torch.save(decoder.model.state_dict(), netPath / 'decoder.pth')
+        print(f'safed to {netPath}')
+
     else:
         dataIter = next(iter(dataloader))
         batch = dataIter[0]
